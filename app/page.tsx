95ff4053003e16e01238c781d715283ab5870d10
Harry-1001 <haruka.ishiga@gmail.com>
2025-03-04 22:26:00 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center w-full text-white">
-      <h1 className="text-8xl font-bold">Hello, Next.js!</h1>
-      <p className="mt-6 text-5xl">
-        <Link href={"/weather"} className="text-5xl text-blue-200 underline">
+      <h1 className="text-6xl font-bold">Hello, Next.js!</h1>
+      <p className="mt-6 text-3xl">
+        <Link href={"/weather"} className="text-3xl text-blue-200 underline">
           天気ページへ移動
         </Link>
       </p>
