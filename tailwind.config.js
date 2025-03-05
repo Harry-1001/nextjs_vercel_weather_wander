a69e7596ff8b6f1ca43caea5d443971e4bed03aa
Haruka Ishiga <haruka.ishiga@gmail.com>
2025-03-05 16:36:10 +0900

    tailwaind config.js 修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,32 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        sunny: 'sunshine 3s ease-in-out infinite',
+        rainy: 'rainy 3s ease-in-out infinite',
+        cloudy: 'cloudy 3s ease-in-out infinite',
+        snowy: 'snowy 3s ease-in-out infinite',
+      },
+      keyframes: {
+        sunshine: {
+          '0%': { backgroundColor: '#FFEB3B' }, // 黄色
+          '100%': { backgroundColor: '#FF9800' }, // オレンジ
+        },
+        rainy: {
+          '0%': { backgroundColor: '#607d8b' }, // グレー
+          '100%': { backgroundColor: '#1a237e' }, // 濃い青
+        },
+        cloudy: {
+          '0%': { backgroundColor: '#bdbdbd' }, // ライトグレー
+          '100%': { backgroundColor: '#424242' }, // ダークグレー
+        },
+        snowy: {
+          '0%': { backgroundColor: '#ffffff' }, // 白
+          '100%': { backgroundColor: '#cfd8dc' }, // 灰色
+        },
+      },
+    },
   },
   plugins: [],
 }
